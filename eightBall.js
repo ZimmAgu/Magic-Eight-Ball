8092e533576e3cb41c73b7559cc5af0caf35c996
Zim Agu <zimmagu@gmail.com>
2021-01-04 10:32:51 -0800

    Update eightBall.js
    The magic eight ball project is now finished

--- eightBall.js
+++ eightBall.js
@@ -27,6 +27,10 @@ userName ? console.log(`Hello ${userName}!`)
 
 
 
+
+
+
+
 // This is the question that is being asked
 let userQuestion = 'Will Drake drop his album this month?';
 
@@ -40,13 +44,30 @@ console.log(`${userName} asked the question ${userQuestion}`);
 
 
 
+/*  This space separates the users question 
+    and the eight ball's answer */
+console.log(``);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*  
     Math.random * 8 generates a random number between 0 & 8
 
     Math.floor ensures the we can only have whole numbers
     by rounding down    
 */
-Math.floor(Math.random() * 8);
+randomNumber = Math.floor(Math.random() * 8);
 
 
 
@@ -54,4 +75,62 @@ Math.floor(Math.random() * 8);
 
 
 
-eightBall = ` `
+
+
+
+let eightBall = ` `
+
+
+
+
+
+
+
+
+
+/*  
+    This switch statement choose what Magic Eight ball term 
+    will be printed to the screen
+
+    The case number is decided by the randomNumber variable
+*/
+switch (randomNumber) {
+
+    case 0:
+        eightBall =`It is certain`;
+        break;
+
+    case 1:
+        eightBall = `It is decidedly so`;
+        break;
+
+    case 2:
+        eightBall = `Reply hazy try again`;
+        break;
+
+    case 3:
+        eightBall = `cannot predict now`;
+        break;
+
+    case 4:
+        eightBall = `Do not count on it`;
+        break;
+
+    case 5:
+        eightBall = `My sources say no`;
+        break;
+
+    case 6:
+        eightBall = `Outlook not so good`;
+        break;
+
+    case 7:
+        eightBall = `Signs point to yes`;
+        break;
+
+} // End of switch statement
+ 
+
+//  Prints the response of the magic eight ball
+console.log(`The magic eight ball says:`);
+console.log(`${eightBall}`);
